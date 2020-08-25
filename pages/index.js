5b426ab18591f134ec1546569f110c3a2736b9c3
Toma Otani <otani.toma@gmail.com>
2020-08-25 12:11:02 -0700

    Polishing Layout

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Irure est qui dolor occaecat dolore cupidatat commodo aute non culpa exercitation ea nostrud. Dolore ea cupidatat consequat ex ut culpa pariatur laborum dolor reprehenderit. Dolor qui qui exercitation mollit cupidatat dolore proident esse eu reprehenderit ad. Et minim laboris aliqua deserunt aliqua amet aute. Proident do consectetur laborum consequat. Fugiat consequat non consectetur irure in proident anim est incididunt excepteur non exercitation. Sint dolore laborum nisi deserunt ullamco adipisicing non laboris elit amet.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
